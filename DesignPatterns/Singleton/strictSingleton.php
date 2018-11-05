b5bc00412edebbbdd90001c5493e693017f5091f
weiqixu <weiqixuzj@163.com>
2018-11-05 12:02:03 +0800

    单例更新

--- DesignPatterns/Singleton/strictSingleton.php
+++ DesignPatterns/Singleton/strictSingleton.php
@@ -3,24 +3,19 @@
 *
 * 单例模式：确保一类只有一个实例。
 *
-*
-* 普通的单例在clone时，仍然会出现两个单例的情况，
-* 在将__clone()魔术方法改为private后可杜绝此情况。
 */
 class Singleton
 {
     static private $instance = null;
-    private function __construct()
-    {}
+    private function __construct() {}
+    private function __wakeup() {}
+    private function __clone() {}
     static public function getInstance()
     {
         if (is_null(static::$instance)) {
             static::$instance = new self;
         }
         return static::$instance;
-    }
-    private function __clone()
-    {
     }
 }
 class Test
