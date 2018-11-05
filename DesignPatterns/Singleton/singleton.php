211687a16c809f632178e6ce1b513a1c34b55b19
weiqixu <weiqixuzj@163.com>
2018-11-05 12:00:31 +0800

    单例更新

--- DesignPatterns/Singleton/singleton.php
+++ DesignPatterns/Singleton/singleton.php
@@ -2,15 +2,16 @@
 /*
 * 单例模式：确保一类只有一个实例。
 *
-* 下面是单例模式的一个示例，通过将构造方法置为私有，保证用户不能在任意地方使用new来随意创建对象。
+* 下面是单例模式的一个示例，通过将构造方法置为私有，保证用户不能在任意地方使用new、clone、serialize来随意创建对象。
 * 提供一个获取实例的静态方法，在方法中判断实例是否已经被实例化过，如果有已经实例化的对象，则返回此对象，反之创建对象。
 * 从而保证全局中该类只有一个实例存在。
 */
 class Singleton
 {
     static private $instance = null;
-    private function __construct()
-    {}
+    private function __construct() {}
+    private function __wakeup() {}
+    private function __clone() {}
     static public function getInstance()
     {
         if (is_null(static::$instance)) {
